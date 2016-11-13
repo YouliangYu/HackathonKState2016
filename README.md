c058476c7b3b6548f7b88bed0ecb3e805a464345
YouliangYu <youliang1229@gmail.com>
2016-11-13 12:21:16 -0600

    commit for submission

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Hack K-State 2016: Data Science Challenge For Social Good
 
-In this challenge(https://zslie.github.io/), we build BetterDAI: a web application that leverages hardcore
-data analysis to ensure hospital patients are treated correct.
+In this hackathon challenge(https://zslie.github.io/), we build BetterDAI: a web application that apply hardcore
+data analysis and machine learning to ensure hospital patients are treated correct.
 
-Our goal was to predict which patients will be re-admitted to the hospital within 30 days of their discharge. 
+The goal is to predict which patients will be re-admitted to the hospital within 30 days of their discharge. 
 This is a real problem for hospitals, who don't get paid for a re-admission if it happens within 30 days 
 after the patient was discharged, and patients, who would like to live their life outside the walls of 
 a hospital and be healthy.
 
-This repo includes the back end machine learning models that actually perform the prediction.    
+This repo includes the back end machine learning algorithms that carry out the prediction.    
 
-The full app repo could be found at https://github.com/srjayhawkfan/HackKState2016 
+The full web app repo could be found at https://github.com/srjayhawkfan/HackKState2016 
 
--- single_molel.R
+++ single_molel.R
@@ -11,11 +11,12 @@ library(Metrics)
 
 train <- fread('data/Challenge_1_Training.csv',header =TRUE,stringsAsFactors = FALSE,
                na.strings=c("?","No","NO","None",""),data.table= FALSE)
-test <- fread('data/Challenge_1_Testing.csv',header =TRUE,stringsAsFactors = FALSE,
+test <- fread('test.csv',header =TRUE,stringsAsFactors = FALSE,
                na.strings=c("?","No","NO","None",""),data.table= FALSE)
 train <- train[!is.na(train$readmitted),] #rm label NA
 y <- as.integer(as.factor(train$readmitted))-1
 train <- train[,!names(train) %in% c("encounter_id","patient_nbr","readmitted")] #remove IDs 
+testID <- test$patient_nbr
 test <- test[,!names(test) %in% c("encounter_id","patient_nbr")] 
 
 # factorize char vars
@@ -33,35 +34,23 @@ cst_vars <- names(train)[apply(train, 2, function(x) length(unique(x))==1)]
 for (f in cst_vars){train[[f]] <- NULL; test[[f]] <- NULL}
 
 # remove features with all NA(>99.99%)
-na_var <- names(train)[apply(train, 2, function(x)  sum(is.na(x))/nrow(train)>0.999)]
+na_var <- names(train)[apply(train, 2, function(x)  sum(is.na(x))/nrow(train)>0.9999)]
 for (f in na_var){train[[f]] <- NULL; test[[f]] <- NULL}
 
 # impute NA
-train[is.na(train)] <- 0
-
-#one-hot-encode
-encode_vars <- names(train)[apply(rbind(train), 2, function(x) length(unique(x))<100)]
-for (f in intersect(names(train),union(encode_vars,fact_vars))){ 
-  dummyfact <- as.factor(c(-100000,train[[f]],testx[[f]]))
-  temp <- model.matrix(~dummyfact)
-  k=0
-  for (j in 2:ncol(temp)){
-    colnames(temp)[j] <- paste0(f,"Dummy",k)
-    k <- k+1
-  }
-  train <- cbind(train,temp[2:(nrow(train)+1),-1])
-  test <- cbind(test,temp[(nrow(test)+2):nrow(temp),-1])
-  rm(temp);rm(dummyfact);gc()
-}
+train[is.na(train)] <- 0; test[is.na(test)] <- 0
 
 #build xgb model
 set.seed(1229);dtrain <- xgb.DMatrix(as.matrix(train), label = y)
 param <- list(booster ="gbtree", objective = 'binary:logistic', eval_metric = 'rmse',
-              nthread = 6,eta = 0.02, colsample_bytree = 0.4, subsample = 0.8, max_depth = 6, min_child_weight=11)
+              nthread = 6,eta = 0.01, colsample_bytree = 0.4, subsample = 0.8, max_depth = 6, min_child_weight=11)
 #bst.cv = xgb.cv(param=param, data = dtrain, nfold = 5, nrounds = 5000, early.stop.round = 50)
 #[460]	train-rmse:0.402558+0.000524	test-rmse:0.419567+0.003207
 #tmp <- bst.cv$test.rmse.mean; n=which(tmp==min(tmp))[1]
-bst <- xgb.train(params=param, data=dtrain, nround = as.integer(460/0.8))
+n=913;bst <- xgb.train(params=param, data=dtrain, nround = as.integer(n/0.8))
 pred <- predict(bst, as.matrix(test));print(pred)
 
- 
+#output csv
+submission <- data.frame(ID=testID, readmitted= as.numeric(format(pred,digits = 16,scientific = FALSE)))
+head(submission)
+write.csv(submission, "test_readmitted.csv", row.names=FALSE)
